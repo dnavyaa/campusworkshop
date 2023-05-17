23b3aa913502c4fda20b1637d1995913ecb42d63
dnavyaa <dhankarnavya22@gmail.com>
2023-05-17 14:49:39 +0530

    newnew

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import psycopg2
 
 app = Flask(__name__)
 postgres = psycopg2.connect(
-        host="postgres://root:xyk9mRUPOh8m3jnJTZ6ZsRKdJY6fcKH2@dpg-chi85el269vf5qbest50-a.oregon-postgres.render.com/todo_gguq",
+        host="dpg-chi85el269vf5qbest50-a.oregon-postgres.render.com",
         database="todo_gguq",
         user="root",
         password="xyk9mRUPOh8m3jnJTZ6ZsRKdJY6fcKH2")
